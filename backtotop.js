1be6a947990e88fb0689bbae5a304ef53710ef40
CHEN-YU LEE <cllee2@student.unimelb.edu.au>
2025-05-30 17:29:56 +1000

    desktop homepage implementation

--- backtotop.js
+++ backtotop.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const button = document.getElementById('backToTop');
-    button.style.cursor = 'pointer';
-    button.addEventListener('click', () => {
+    document.querySelectorAll('.back_to_top').forEach(button => {
+      button.style.cursor = 'pointer';
+      button.addEventListener('click', () => {
         window.scrollTo({ top: 0, behavior: 'smooth' });
+      });
     });
-});
+  });
